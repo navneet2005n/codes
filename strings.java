af6a2a92d51d96c494b46f9b101d42aba984f41b
Navneet Kumar <navneetkumar2111@gmail.com>
2025-01-27 18:33:41 +0530

    df

--- strings.java
+++ strings.java
@@ -6,6 +6,7 @@
 // When you use split(""), it splits the string into individual characters, including spaces.
 // This will break the entire string into its constituent characters, which means every single character, including spaces, will be treated as a separate element.
 
+import java.util.*;
 public class strings {
     public static int cntpalindrome(String str){
         String words []= str.split(" ");
@@ -27,7 +28,43 @@ public class strings {
         }
         return true;
     }
+
+    
+// Swap first and last character of each word in a string ::::
+
+    public static String swap(String str){
+        String result = "";
+        String words[]= str.split(" ");
+        for(String word:words){
+        char firstchar = word.charAt(0);
+        char lastchar =  word.charAt(word.length()-1);
+            if(str.length()>1){
+                result += lastchar + word.substring(1, word.length()-1) + firstchar+" " ; 
+            }
+            else{
+                result+=word+" ";
+            }
+        }
+        return result.trim();
+    }
+    public static String capifirstchar(String str){
+        String result = "";
+        String words[] = str.split(" ");
+        for(String word: words){
+            if(!word.isEmpty()){
+                word = word.substring(0,1).toUpperCase()+word.substring(1)+" ";
+            }
+            result +=word;
+        }
+        return result.trim();
+    }
+
     public static void main(String args[]){
         String str = "Mom and Dad are my best friend";
         System.out.println(cntpalindrome(str));
-}}
+        System.out.println(swap(str));
+        System.out.println(capifirstchar(str));
+}
+}
+
+
