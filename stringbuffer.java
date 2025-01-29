beadd9c8bf0c0401ce498611d3c8afac7382d5e5
Navneet Kumar <navneetkumar2111@gmail.com>
2025-01-29 17:01:10 +0530

    tr

--- stringbuffer.java
+++ stringbuffer.java
@@ -3,9 +3,12 @@ public class stringbuffer {
         StringBuffer inst = new StringBuffer("hello");
         // inst.append(" wrld");
         inst.insert(5," wrld");
+        inst.replace(5,10," java");
+        inst.delete(6, 9);
         System.out.println(inst);
     }
 }
+
 
 // The length of "hello" is 5, so:
 // Valid indices for insert(index, "text") are 0 to 5 (inclusive).
@@ -14,3 +17,9 @@ public class stringbuffer {
 // ✅ Valid indices = 0 to length of string
 // ❌ Invalid index = Anything greater than string.length()
 
+
+// sb.delete(6, 9);
+// the delete(int start, int end) method removes characters from index start to end - 1, meaning index 9 is NOT included.
+// How It Works?
+// start (inclusive): Characters starting from index 6 are deleted.
+// end (exclusive): Characters up to index 9 (but not including it) are deleted.
