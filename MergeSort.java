12714a43e678333ff11ef14b33772476722521f9
Navneet Kumar <navneetkumar2111@gmail.com>
2025-02-23 14:41:31 +0530

    df

--- MergeSort.java
+++ MergeSort.java
@@ -1,22 +1,22 @@
 import java.util.Arrays;
 public class MergeSort {
-    public static int[] mergeSort(int arr[], int l, int r) {
-        if (l >= r) { 
-            return new int[]{arr[l]};
-        }
-        int mid = (l + r) / 2;
-        int left[] = mergeSort(arr, l, mid);
-        int right[] = mergeSort(arr, mid + 1, r);
+    public static int[] mergeSort(int arr[]) {
+        if (arr.length == 1) {
+            return arr;
+        }   
+        int mid = arr.length / 2;
+        int left[] = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int right[] = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
         return merge(left, right);
     }
- 
-    private static int[] merge(int[] left, int[] right) {
+
+    public static int[] merge(int[] left, int[] right) {
         int i = 0, j = 0, k = 0;
         int[] result = new int[left.length + right.length];
         while (i < left.length && j < right.length) {
             if (left[i] < right[j]) {
                 result[k++] = left[i++];
-            } else {
+            } else { 
                 result[k++] = right[j++];
             }
         }
@@ -30,8 +30,9 @@ public class MergeSort {
     }
     public static void main(String args[]) {
         int arr[] = {5, 4, 3, 2, 1};
-        int n = arr.length;
-        arr = mergeSort(arr, 0, n - 1); 
+        // it is creating a copy of real array not doing it on real array....
+        mergeSort(arr);   // this will not sort the array as it is not doing it on real array
+        arr = mergeSort(arr);  // this will sort the array as it is doing it on real array
         System.out.println(Arrays.toString(arr));
     }
 }
