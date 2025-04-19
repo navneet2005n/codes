9bb24110d87335a85d18c4ef3ea5007aeddfee0a
Navneet Kumar <navneetkumar2111@gmail.com>
2025-04-19 23:37:34 +0530

    ppo

--- bubble_sort.java
+++ bubble_sort.java
@@ -4,7 +4,7 @@ public  class bubble_sort{
         int n=arr.length;
         for(int i=0;i<n;i++){
             for(int j=0;j<n-i-1;j++){
-                if(arr[j]>arr[j+1]){
+                if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
@@ -12,6 +12,7 @@ public  class bubble_sort{
             }
         }
     }
+
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements: ");
